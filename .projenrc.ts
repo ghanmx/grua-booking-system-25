44bcdfa7bc70e1b57a68d13f26d6049b50e1382a
ghanmx <91572299+ghanmx@users.noreply.github.com>
2024-07-21 13:29:40 +0000

    @ghanmx

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,48 +1,48 @@
 import { ProjenBlueprint } from '@amazon-codecatalyst/blueprint-util.projen-blueprint';
 
 const project = new ProjenBlueprint({
-  "authorName": "pruebadeamazon",
-  "publishingOrganization": "pruebadeamazon",
-  "packageName": "@amazon-codecatalyst/pruebadeamazon.my-amazing-blueprint",
-  "name": "my-amazing-blueprint",
-  "displayName": "My Amazing Blueprint",
-  "defaultReleaseBranch": "main",
-  "license": "Apache-2.0",
-  "projenrcTs": true,
-  "sampleCode": false,
-  "github": false,
-  "eslint": true,
-  "jest": false,
-  "npmignoreEnabled": true,
-  "tsconfig": {
-    "compilerOptions": {
-      "esModuleInterop": true,
-      "noImplicitAny": false
-    }
+  authorName: 'pruebadeamazon',
+  publishingOrganization: 'pruebadeamazon',
+  packageName: '@amazon-codecatalyst/pruebadeamazon.my-amazing-blueprint',
+  name: 'my-amazing-blueprint',
+  displayName: 'My Amazing Blueprint',
+  defaultReleaseBranch: 'main',
+  license: 'Apache-2.0',
+  projenrcTs: true,
+  sampleCode: false,
+  github: false,
+  eslint: true,
+  jest: false,
+  npmignoreEnabled: true,
+  tsconfig: {
+    compilerOptions: {
+      esModuleInterop: true,
+      noImplicitAny: false,
+    },
   },
-  "copyrightOwner": "pruebadeamazon",
-  "deps": [
-    "projen",
-    "@amazon-codecatalyst/blueprints.blueprint",
-    "@amazon-codecatalyst/blueprint-component.workflows",
-    "@amazon-codecatalyst/blueprint-component.source-repositories",
-    "@amazon-codecatalyst/blueprint-component.dev-environments",
-    "@amazon-codecatalyst/blueprint-component.environments",
-    "@amazon-codecatalyst/blueprint-component.issues"
+  copyrightOwner: 'pruebadeamazon',
+  deps: [
+    'projen',
+    '@amazon-codecatalyst/blueprints.blueprint',
+    '@amazon-codecatalyst/blueprint-component.workflows',
+    '@amazon-codecatalyst/blueprint-component.source-repositories',
+    '@amazon-codecatalyst/blueprint-component.dev-environments',
+    '@amazon-codecatalyst/blueprint-component.environments',
+    '@amazon-codecatalyst/blueprint-component.issues',
   ],
-  "description": "This blueprint creates an empty application.",
-  "devDeps": [
-    "ts-node@^10",
-    "typescript",
-    "@amazon-codecatalyst/blueprint-util.projen-blueprint",
-    "@amazon-codecatalyst/blueprint-util.cli",
-    "fast-xml-parser"
+  description: 'This blueprint creates an empty application.',
+  devDeps: [
+    'ts-node@^10',
+    'typescript',
+    '@amazon-codecatalyst/blueprint-util.projen-blueprint',
+    '@amazon-codecatalyst/blueprint-util.cli',
+    'fast-xml-parser',
   ],
-  "keywords": [
-    "first-label",
-    "second-label"
+  keywords: [
+    'first-label',
+    'second-label',
   ],
-  "homepage": ""
+  homepage: '',
 });
 
 project.synth();
